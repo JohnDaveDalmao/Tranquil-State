6308cad07ddcc0e54f5664a636e884bef94f982c
JohnDaveDalmao <jhav3dalx@gmail.com>
2020-05-21 17:27:37 +0800

    Nav working without click event

---
+++ js/nav.js
@@ -0,0 +1,7 @@
+// document.getElementById("menu-icon").addEventListener("click", function hey() {
+//   document.getElementById("toggled-nav").style.visibility = "visible";
+// });
+
+// function hey() {
+//   document.getElementById("toggled-nav").style.visibility = "visible";
+// }
