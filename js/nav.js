676af2f63129130b2015ed35c781cc792df16cec
JohnDaveDalmao <jhav3dalx@gmail.com>
2020-05-21 17:54:35 +0800

    Completed Index Mobile View

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,9 @@
-// document.getElementById("menu-icon").addEventListener("click", function hey() {
-//   document.getElementById("toggled-nav").style.visibility = "visible";
-// });
+function ShowNav() {
+  document.getElementById("toggled-nav").style.visibility = "visible";
+  document.getElementById("header").style.visibility = "hidden";
+}
 
-// function hey() {
-//   document.getElementById("toggled-nav").style.visibility = "visible";
-// }
+function HideNav() {
+  document.getElementById("toggled-nav").style.visibility = "hidden";
+  document.getElementById("header").style.visibility = "visible";
+}
